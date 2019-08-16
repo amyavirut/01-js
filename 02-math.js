9b977e4818b80cf458e44c463f6f26fa0eead129
Amy Avirut <amyavirut@gmail.com>
2019-08-16 17:53:48 +0700

    done math

--- 02-math.js
+++ 02-math.js
@@ -6,19 +6,40 @@ let numBers = 3.6
 let returnNumber = (b) => {
     console.log(Math.round(b))
 }
+console.log("returnNumber")
 returnNumber(numBers)
 
 //20202
 let returnPrice = (b) => {
-    Math.round(b.price) 
+    return b.map((s) => {
+        s.price = Math.round(s.price)
+        return s
+    })
 }
-returnPrice(products)
-console.log(returnPrice)
-
+console.log("returnPrice")
+console.log(returnPrice(products))
 
 //20203
-let twoPara = (a, b) => {
-    return 
+let twoPara = (input, places) => {
+    return Number.parseFloat(input).toFixed(places)
 }
-twoPara(2.34, 5.89)
+console.log(twoPara(2.324, 1))
 console.log(twoPara)
+
+//20204 
+let powerTwo = (base, exp) => {
+    return Math.pow(base, exp)
+}
+console.log(powerTwo(2, 2))
+
+//20205
+let randomNum = () => {
+    return Math.round(Math.random() * 10)
+}
+console.log(randomNum()) 
+
+//20206
+let randomNumP = (p) => {
+    return Math.round(Math.random() * p)
+}
+console.log(randomNumP(35)) 
